ca1f9a08d929ed56730f733551e0cf8e31cdfa63
Raynara Santiago <raysantiagobz@gmail.com>
2023-02-17 13:15:15 -0300

    req 07

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,5 @@
-from typing import List, Dict
+from typing import Dict, List
+
 from .jobs import read
 
 
@@ -12,18 +13,8 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    jobFilterIndustry = []
+    for job in jobs:
+        if job['industry'] == industry:
+            jobFilterIndustry.append(job)
+    return jobFilterIndustry

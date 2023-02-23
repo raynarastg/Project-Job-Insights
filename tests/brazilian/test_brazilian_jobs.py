8c139ba44ab7c76c527f49af98cab706c0e26e36
Raynara Santiago <raysantiagobz@gmail.com>
2023-02-23 13:42:29 -0300

    req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,6 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from tests.brazilian.mock import mock
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file('tests/mocks/brazilians_jobs.csv') == mock
